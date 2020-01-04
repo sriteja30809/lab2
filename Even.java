6b0fa90a4c92a2deaf4a5b3c36ba6210ac9565f8
sriteja30809 <59111266+sriteja30809@users.noreply.github.com>
2020-01-04 14:09:47 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Even
+{
+public static void main(String []args)
+{
+int num=3;
+if(num%2==0)
+{
+System.out.println("even");
+}
+else{
+System.out.println("odd");
+}
+}
+}
